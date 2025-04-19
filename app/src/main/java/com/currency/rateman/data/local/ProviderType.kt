63d0f73feecb567305b918f5574f1d0a934822e1
Rakhman <karymshakovv@gmail.com>
2025-04-19 10:44:06 +0200

    Add ProviderType enum class. Add ratesDate and providerType attributes to RateProvider class. Add ratesDate and type attributes to RateProviderEntity class.

---
+++ app/src/main/java/com/currency/rateman/data/local/ProviderType.kt
@@ -0,0 +1,7 @@
+package com.currency.rateman.data.local
+
+enum class ProviderType {
+    BANK,
+    EXCHANGE,
+    CRYPTO_EXCHANGE
+}
--- app/src/main/java/com/currency/rateman/data/local/RateProvider.kt
+++ app/src/main/java/com/currency/rateman/data/local/RateProvider.kt
@@ -4,5 +4,7 @@ data class RateProvider(
     val name: String,
     val baseCurrency: CurrencyCode,
     val rates: List<CurrencyRate>,
+    val ratesDate: String,
     val phoneNumber: String,
+    val providerType: ProviderType,
 )
