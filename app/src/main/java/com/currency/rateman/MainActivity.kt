e2e66528483ae72a97491d8abe907e42c125b339
Rakhman <karymshakovv@gmail.com>
2025-04-21 19:51:48 +0200

    Rename PlaygroundAppTheme to RateManAppTheme and update its usage in MainActivity.

--- app/src/main/java/com/currency/rateman/MainActivity.kt
+++ app/src/main/java/com/currency/rateman/MainActivity.kt
@@ -5,14 +5,14 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import com.currency.rateman.ui.navigation.AppRouter
-import com.currency.rateman.ui.theme.PlaygroundAppTheme
+import com.currency.rateman.ui.theme.RateManAppTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            PlaygroundAppTheme {
+            RateManAppTheme {
                 AppRouter()
             }
         }
