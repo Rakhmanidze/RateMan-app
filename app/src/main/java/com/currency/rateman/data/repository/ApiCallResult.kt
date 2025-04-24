ee15ed66d8970802be12bf35f8e30c8d56e8e0e1
Rakhman <karymshakovv@gmail.com>
2025-04-24 15:52:24 +0200

    Remove unneeded files.

--- app/src/main/java/com/currency/rateman/data/repository/ApiCallResult.kt
+++
@@ -1,7 +0,0 @@
-package com.currency.rateman.data.repository
-
-sealed class ApiCallResult<out T> {
-    data object Loading : ApiCallResult<Nothing>()
-    data class Success<T>(val data: T) : ApiCallResult<T>()
-    data class Error(val exception: Throwable) : ApiCallResult<Nothing>()
-}
