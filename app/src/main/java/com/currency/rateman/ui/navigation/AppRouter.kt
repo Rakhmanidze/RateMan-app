256517962735266ad734eb333e5c61d9db8354db
Rakhman <karymshakovv@gmail.com>
2025-04-27 10:55:32 +0200

    Refactor AppRouter by removing unused imports and variables.

--- app/src/main/java/com/currency/rateman/ui/navigation/AppRouter.kt
+++ app/src/main/java/com/currency/rateman/ui/navigation/AppRouter.kt
@@ -1,21 +1,13 @@
 package com.currency.rateman.ui.navigation
 
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
-import androidx.compose.runtime.saveable.rememberSaveable
-import androidx.compose.runtime.setValue
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.currentBackStackEntryAsState
 import androidx.navigation.compose.rememberNavController
 import com.currency.rateman.R
-import com.currency.rateman.data.model.CurrencyCode
-import com.currency.rateman.data.model.LanguageCode
-import com.currency.rateman.data.model.Profile
-import com.currency.rateman.data.model.ThemeMode
 import com.currency.rateman.ui.screens.ProfileScreen
 import com.currency.rateman.ui.screens.RatesScreen
 import com.currency.rateman.ui.screens.Splashscreen
