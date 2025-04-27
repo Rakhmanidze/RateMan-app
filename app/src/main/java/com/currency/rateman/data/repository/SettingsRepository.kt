69b4e32f718aa7d525c921931693ab84b41683d7
Rakhman <karymshakovv@gmail.com>
2025-04-27 12:51:48 +0200

    Add SettingsRepository and SettingsRepositoryFake. Implement getSettings, saveSettings, and resetSettings functions.

--- app/src/main/java/com/currency/rateman/data/repository/SettingsRepository.kt
+++ app/src/main/java/com/currency/rateman/data/repository/SettingsRepository.kt
@@ -1,4 +1,10 @@
 package com.currency.rateman.data.repository
 
+import com.currency.rateman.data.model.Settings
+import kotlinx.coroutines.flow.Flow
+
 interface SettingsRepository {
+    fun getSettings(): Flow<Settings>
+    suspend fun saveSettings(settings: Settings)
+    suspend fun resetSettings()
 }
--- app/src/main/java/com/currency/rateman/data/repository/SettingsRepositoryFake.kt
+++ app/src/main/java/com/currency/rateman/data/repository/SettingsRepositoryFake.kt
@@ -1,2 +1,33 @@
 package com.currency.rateman.data.repository
 
+import com.currency.rateman.data.model.CurrencyCode
+import com.currency.rateman.data.model.LanguageCode
+import com.currency.rateman.data.model.Settings
+import com.currency.rateman.data.model.ThemeMode
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
+
+class SettingsRepositoryFake : SettingsRepository {
+    private val settingsFlow = MutableStateFlow(
+        Settings(
+            defaultCurrency = CurrencyCode.CZK,
+            uiLanguage = LanguageCode.EN,
+            themeMode = ThemeMode.DARK
+        )
+    )
+
+    override fun getSettings(): Flow<Settings> = settingsFlow.asStateFlow()
+
+    override suspend fun saveSettings(settings: Settings) {
+        settingsFlow.value = settings
+    }
+
+    override suspend fun resetSettings() {
+        settingsFlow.value = Settings(
+            defaultCurrency = CurrencyCode.CZK,
+            uiLanguage = LanguageCode.EN,
+            themeMode = ThemeMode.DARK
+        )
+    }
+}
