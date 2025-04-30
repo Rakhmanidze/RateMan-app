4d15d352db301456ced0ed09489f04032cd95e5e
Rakhman <karymshakovv@gmail.com>
2025-04-30 18:33:21 +0200

    Refactor Koin modules to remove repositoryModule and add SettingsDao. Initialize AppContainer in RateManApp. Use SettingsRepositoryImpl instead of SettingsRepositoryFake.

--- app/src/main/java/com/currency/rateman/RateManApp.kt
+++ app/src/main/java/com/currency/rateman/RateManApp.kt
@@ -2,7 +2,6 @@ package com.currency.rateman
 
 import android.app.Application
 import com.currency.rateman.di.appModule
-import com.currency.rateman.di.repositoryModule
 import org.koin.android.ext.koin.androidContext
 import org.koin.core.context.startKoin
 
@@ -10,9 +9,10 @@ class RateManApp: Application() {
 
     override fun onCreate() {
         super.onCreate()
+        AppContainer.init(applicationContext)
         startKoin {
             androidContext(this@RateManApp)
-            modules(appModule, repositoryModule)
+            modules(appModule)
         }
     }
 }
--- app/src/main/java/com/currency/rateman/di/KoinModules.kt
+++ app/src/main/java/com/currency/rateman/di/KoinModules.kt
@@ -1,20 +1,21 @@
 package com.currency.rateman.di
 
+import com.currency.rateman.AppContainer
 import com.currency.rateman.data.repository.RateProviderRepository
 import com.currency.rateman.data.repository.RateProviderRepositoryFake
 import com.currency.rateman.data.repository.SettingsRepository
-import com.currency.rateman.data.repository.SettingsRepositoryFake
+import com.currency.rateman.data.repository.SettingsRepositoryImpl
 import com.currency.rateman.ui.viewmodels.RatesViewModel
 import com.currency.rateman.ui.viewmodels.SettingsViewModel
 import org.koin.androidx.viewmodel.dsl.viewModel
 import org.koin.dsl.module
 
 val appModule = module {
+    single { AppContainer.rateManDatabase.settingsDao() }
+
+    single<RateProviderRepository> { RateProviderRepositoryFake() }
+    single<SettingsRepository> { SettingsRepositoryImpl(get()) }
+
     viewModel { RatesViewModel(get()) }
     viewModel { SettingsViewModel(get()) }
-}
-
-val repositoryModule = module {
-    single<RateProviderRepository> { RateProviderRepositoryFake() }
-    single<SettingsRepository> { SettingsRepositoryFake() }
 }
