eaffa7968de348369fdaf3de62398a17ce032f64
Rakhman <karymshakovv@gmail.com>
2025-05-08 14:05:57 +0200

    Add Mapper for FilterEntity.

---
+++ app/src/main/java/com/currency/rateman/data/model/Mappers.kt
@@ -0,0 +1,38 @@
+package com.currency.rateman.data.model
+
+import com.currency.rateman.data.db.entity.FilterEntity
+import com.currency.rateman.data.db.entity.SettingsEntity
+
+fun Settings.toEntity() : SettingsEntity {
+    return SettingsEntity(
+        id = 0,
+        defaultCurrency = defaultCurrency.name,
+        uiLanguage = uiLanguage.name,
+        themeMode = themeMode.name
+    )
+}
+
+fun SettingsEntity.toSettings() : Settings {
+    return Settings(
+        defaultCurrency = CurrencyCode.valueOf(defaultCurrency),
+        uiLanguage = LanguageCode.valueOf(uiLanguage),
+        themeMode = ThemeMode.valueOf(themeMode)
+    )
+}
+
+fun Filter.toEntity() : FilterEntity {
+    return FilterEntity(
+        id = 0,
+        selectedProviderType = selectedProviderType.name,
+        selectedCurrency = selectedCurrency.name,
+        selectedRateSortType = selectedRateSortType.name
+    )
+}
+
+fun FilterEntity.toFilter() : Filter {
+    return Filter(
+        selectedProviderType = ProviderType.valueOf(selectedProviderType),
+        selectedCurrency = CurrencyCode.valueOf(selectedCurrency),
+        selectedRateSortType = RateSortType.valueOf(selectedRateSortType)
+    )
+}
