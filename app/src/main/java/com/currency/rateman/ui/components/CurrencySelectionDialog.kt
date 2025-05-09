d1413b4d38d641186fd4bcbc5c3ae4c5de15a185
Rakhman <karymshakovv@gmail.com>
2025-05-09 20:03:00 +0200

    Update CurrencySelectionDialog layout. Add a TextField for searching currencies and use LazyColumn to display the list of currencies. Adjust layout to fill 2/3 of the screen height.

--- app/src/main/java/com/currency/rateman/ui/components/CurrencySelectionDialog.kt
+++ app/src/main/java/com/currency/rateman/ui/components/CurrencySelectionDialog.kt
@@ -3,14 +3,19 @@ package com.currency.rateman.ui.components
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.unit.dp
+import androidx.compose.ui.platform.LocalConfiguration
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.unit.dp
 import com.currency.rateman.R
 import com.currency.rateman.data.model.CurrencyCode
 import androidx.compose.runtime.getValue
@@ -28,77 +33,85 @@ fun CurrencySelectionDialog(
     ModalBottomSheet(
         onDismissRequest = onDismiss,
         modifier = Modifier.fillMaxWidth(),
-        sheetState = rememberModalBottomSheetState(),
+        sheetState = rememberModalBottomSheetState(
+            skipPartiallyExpanded = true
+        ),
         windowInsets = WindowInsets(0),
         containerColor = MaterialTheme.colorScheme.surfaceVariant
     ) {
         Column(
             modifier = Modifier
                 .fillMaxWidth()
-                .padding(16.dp)
+                .height((LocalConfiguration.current.screenHeightDp * 2/3).dp)
+                .fillMaxHeight()
+                .padding(16.dp),
+            verticalArrangement = Arrangement.Top
         ) {
             var searchCurrency by remember { mutableStateOf("") }
+
+            Text(
+                text = title,
+                style = MaterialTheme.typography.titleMedium,
+                modifier = Modifier.fillMaxWidth().wrapContentWidth(Alignment.CenterHorizontally)
+            )
+
             TextField(
                 value = searchCurrency,
                 onValueChange = { searchCurrency = it },
                 placeholder = { Text("Search currencies")},
                 modifier = Modifier.fillMaxWidth(),
-                maxLines = 1
+                maxLines = 1,
+                singleLine = true,
+                keyboardOptions = KeyboardOptions(imeAction = ImeAction.Search)
             )
-            Row(
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .padding(vertical = 9.dp),
-                verticalAlignment = Alignment.CenterVertically
-            ) {
-                Text(
-                    text = title,
-                    style = MaterialTheme.typography.titleMedium
-                )
-            }
 
             val filteredOptions = options.filter {
                 it.name.contains(searchCurrency, ignoreCase = true)
             }
 
-            if (filteredOptions.isEmpty()) {
-                Text(
-                    text = "No currencies found",
-                    modifier = Modifier.padding(vertical = 16.dp)
-                )
-            }
-
-            filteredOptions.forEach { option ->
-                Row(
-                    modifier = Modifier
-                        .fillMaxWidth()
-                        .clickable {
-                            onOptionSelected(option)
-                            onDismiss()
-                        }
-                        .padding(vertical = 9.dp),
-                    verticalAlignment = Alignment.CenterVertically
-                ) {
-                    Image(
-                        painter = painterResource(id = getCurrencyIconRes(option)),
-                        contentDescription = "${option.name} icon",
-                        modifier = Modifier.size(24.dp)
-                    )
-                    Spacer(modifier = Modifier.width(8.dp))
-                    Text(
-                        text = option.toString(),
-                        style = MaterialTheme.typography.bodyMedium,
-                        modifier = Modifier.weight(1f)
-                    )
-                    if (option == selectedOption) {
-                        Icon(
-                            painter = painterResource(id = R.drawable.select),
-                            contentDescription = "Selected",
-                            modifier = Modifier.size(24.dp)
+            LazyColumn(Modifier.weight(1f)) {
+                if (filteredOptions.isEmpty()) {
+                    item {
+                        Text(
+                            text = "No currencies found",
+                            modifier = Modifier.padding(vertical = 16.dp)
                         )
                     }
+                } else {
+                    items(filteredOptions) { option ->
+                        Row(
+                            modifier = Modifier
+                                .fillMaxWidth()
+                                .clickable {
+                                    onOptionSelected(option)
+                                    onDismiss()
+                                }
+                                .padding(vertical = 9.dp),
+                            verticalAlignment = Alignment.CenterVertically
+                        ) {
+                            Image(
+                                painter = painterResource(id = getCurrencyIconRes(option)),
+                                contentDescription = "${option.name} icon",
+                                modifier = Modifier.size(24.dp)
+                            )
+                            Spacer(modifier = Modifier.width(8.dp))
+                            Text(
+                                text = option.toString(),
+                                style = MaterialTheme.typography.bodyMedium,
+                                modifier = Modifier.weight(1f)
+                            )
+                            if (option == selectedOption) {
+                                Icon(
+                                    painter = painterResource(id = R.drawable.select),
+                                    contentDescription = "Selected",
+                                    modifier = Modifier.size(24.dp)
+                                )
+                            }
+                        }
+                        Divider()
+                    }
+
                 }
-                Divider()
             }
             Spacer(modifier = Modifier.height(16.dp))
         }
