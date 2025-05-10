1362c6e30156d4b28e2b9f2e7e0d0e951761d529
Rakhman <karymshakovv@gmail.com>
2025-05-10 13:02:18 +0200

    Modify ProviderDetailsScreen: add close button to close the screen.

--- app/src/main/java/com/currency/rateman/ui/screens/ProviderDetailsScreen.kt
+++ app/src/main/java/com/currency/rateman/ui/screens/ProviderDetailsScreen.kt
@@ -6,6 +6,7 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.statusBars
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Clear
@@ -39,13 +40,15 @@ fun ProviderDetailsScreen(
     Column(
         modifier = Modifier
             .fillMaxSize()
+            .padding(top = 24.dp)
             .padding(16.dp)
     ) {
         Icon(
             imageVector = Icons.Default.Clear,
             contentDescription = "Back",
             modifier = Modifier
-                .padding(top = 16.dp)
+                .align(Alignment.End)
+                .size(32.dp)
                 .clickable { navController.popBackStack() }
         )
     }
