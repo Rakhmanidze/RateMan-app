8973e1c470a099d4db6d9b106bcbc8fada28c677
Rakhman <karymshakovv@gmail.com>
2025-05-11 08:03:41 +0200

    Set locale according to settings on app start and on configuration changed.

--- app/src/main/java/com/currency/rateman/RateManApp.kt
+++ app/src/main/java/com/currency/rateman/RateManApp.kt
@@ -1,11 +1,20 @@
 package com.currency.rateman
 
 import android.app.Application
+import android.content.res.Configuration
+import com.currency.rateman.data.model.LanguageCode
+import com.currency.rateman.data.repository.SettingsRepository
 import com.currency.rateman.di.appModule
+import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.runBlocking
+import org.koin.android.ext.android.inject
 import org.koin.android.ext.koin.androidContext
+import org.koin.core.component.KoinComponent
 import org.koin.core.context.startKoin
+import java.util.*
 
-class RateManApp: Application() {
+class RateManApp: Application(), KoinComponent {
+    private val settingsRepository: SettingsRepository by inject()
 
     override fun onCreate() {
         super.onCreate()
@@ -14,5 +23,27 @@ class RateManApp: Application() {
             androidContext(this@RateManApp)
             modules(appModule)
         }
+        updateAppLocale()
+    }
+
+    override fun onConfigurationChanged(newConfig: Configuration) {
+        super.onConfigurationChanged(newConfig)
+        updateAppLocale()
+    }
+
+    private fun updateAppLocale() {
+        val settings = runBlocking { settingsRepository.getSettings().first() }
+        val languageCode = settings.uiLanguage
+
+        val locale = when (languageCode) {
+            LanguageCode.SYSTEM -> Locale.getDefault()
+            LanguageCode.EN -> Locale("en")
+            LanguageCode.CZ -> Locale("cs")
+        }
+
+        Locale.setDefault(locale)
+        val config = Configuration(resources.configuration)
+        config.setLocale(locale)
+        resources.updateConfiguration(config, resources.displayMetrics)
     }
 }
