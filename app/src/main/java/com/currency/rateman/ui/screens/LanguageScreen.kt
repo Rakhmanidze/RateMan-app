e5867d6ac05a52d030b8e0a10fc13a099612a1a5
Rakhman <karymshakovv@gmail.com>
2025-05-14 15:50:08 +0200

    Remove redundant code and add logic to select language by clicking on the language item.

--- app/src/main/java/com/currency/rateman/ui/screens/LanguageScreen.kt
+++ app/src/main/java/com/currency/rateman/ui/screens/LanguageScreen.kt
@@ -5,8 +5,10 @@ import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.material.icons.Icons
@@ -20,12 +22,15 @@ import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.navigation.NavHostController
 import com.currency.rateman.di.navigation.sharedKoinNavViewModel
 import com.currency.rateman.ui.viewmodels.SettingsViewModel
 import com.currency.rateman.R
+import com.currency.rateman.data.model.LanguageCode
 
 @Composable
 fun LanguageScreen(
@@ -63,6 +68,36 @@ fun LanguageScreen(
                         .size(32.dp)
                         .clickable { navController.popBackStack() }
                 )
+            }
+            Spacer(modifier = Modifier.height(16.dp))
+
+            Column {
+                LanguageCode.entries.forEach { language ->
+                    Row(
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .clickable {
+                                viewModel.updateLanguage(context, language)
+                                navController.popBackStack()
+                            }
+                            .padding(vertical = 12.dp),
+                        verticalAlignment = Alignment.CenterVertically,
+                        horizontalArrangement = Arrangement.SpaceBetween
+                    ) {
+                        Text(
+                            text = language.name,
+                            style = MaterialTheme.typography.bodyLarge,
+                            color = MaterialTheme.colorScheme.onSurface
+                        )
+                        if (language == settings?.uiLanguage) {
+                            Icon(
+                                painter = painterResource(id = R.drawable.select),
+                                contentDescription = "Selected",
+                                modifier = Modifier.size(24.dp)
+                            )
+                            }
+                    }
+                }
             }
         }
     }
