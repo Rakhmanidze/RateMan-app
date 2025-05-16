5789e4f79a17fd4e5f50e6a464f4bf974d33bc72
Rakhman <karymshakovv@gmail.com>
2025-05-16 16:08:44 +0200

    Replace Divider with HorizontalDivider.
    The `Divider` composable has been deprecated and replaced with `HorizontalDivider`. This change updates the `LanguageScreen` and `CurrencyScreen` to use the new `HorizontalDivider` composable.

--- app/src/main/java/com/currency/rateman/ui/screens/CurrencyScreen.kt
+++ app/src/main/java/com/currency/rateman/ui/screens/CurrencyScreen.kt
@@ -18,7 +18,6 @@ import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.KeyboardArrowLeft
-import androidx.compose.material3.Divider
 import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
--- app/src/main/java/com/currency/rateman/ui/screens/LanguageScreen.kt
+++ app/src/main/java/com/currency/rateman/ui/screens/LanguageScreen.kt
@@ -13,7 +13,6 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.KeyboardArrowLeft
-import androidx.compose.material3.Divider
 import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
