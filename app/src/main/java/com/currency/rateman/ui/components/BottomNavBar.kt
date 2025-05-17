99df09cb0c1a08a3c470a1d218c43a158c2189da
Rakhman <karymshakovv@gmail.com>
2025-05-17 10:10:32 +0200

    Update BottomNavBar selected icon and indicator colors.
    The selected icon color is now `MaterialTheme.colorScheme.onSurface` and the indicator color is `MaterialTheme.colorScheme.background`.

--- app/src/main/java/com/currency/rateman/ui/components/BottomNavBar.kt
+++ app/src/main/java/com/currency/rateman/ui/components/BottomNavBar.kt
@@ -4,8 +4,10 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.NavigationBar
 import androidx.compose.material3.NavigationBarItem
+import androidx.compose.material3.NavigationBarItemDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
@@ -32,7 +34,11 @@ fun BottomNavBar(
                         contentDescription = item.contentDescription,
                         modifier = Modifier.size(24.dp)
                     )
-                }
+                },
+                colors = NavigationBarItemDefaults.colors(
+                    selectedIconColor = MaterialTheme.colorScheme.onSurface,
+                    indicatorColor = MaterialTheme.colorScheme.background
+                )
             )
         }
     }
