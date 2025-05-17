a8b0e6f28f160394b1cc0f1d41fc217a13c89811
Rakhman <karymshakovv@gmail.com>
2025-05-17 08:58:15 +0200

    Refine app theme colors and disable dynamic and dark themes by default.
    Modified light and dark theme color palettes in `Color.kt`.
    Updated `RateManAppTheme` in `Theme.kt` to disable dynamic color and default to light theme.

--- app/src/main/java/com/currency/rateman/ui/theme/Theme.kt
+++ app/src/main/java/com/currency/rateman/ui/theme/Theme.kt
@@ -42,9 +42,9 @@ private val LightColorScheme = lightColorScheme(
 
 @Composable
 fun RateManAppTheme(
-    darkTheme: Boolean = isSystemInDarkTheme(),
+    darkTheme: Boolean = false, //isSystemInDarkTheme()
     // Dynamic color is available on Android 12+
-    dynamicColor: Boolean = true,
+    dynamicColor: Boolean = false,
     content: @Composable () -> Unit
 ) {
     val colorScheme = when {
