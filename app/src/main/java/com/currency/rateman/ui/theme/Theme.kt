bccab0124b8fb4ebbccbb0f879c42ab6ad550155
Rakhman <karymshakovv@gmail.com>
2025-05-17 08:58:40 +0200

    Remove unused commented-out color definitions from Theme.kt.

--- app/src/main/java/com/currency/rateman/ui/theme/Theme.kt
+++ app/src/main/java/com/currency/rateman/ui/theme/Theme.kt
@@ -28,16 +28,6 @@ private val LightColorScheme = lightColorScheme(
     surface = LightSurface,
     onPrimary = LightOnPrimary,
     onBackground = LightOnBackground,
-
-    /* Other default colors to override
-    background = Color(0xFFFFFBFE),
-    surface = Color(0xFFFFFBFE),
-    onPrimary = Color.White,
-    onSecondary = Color.White,
-    onTertiary = Color.White,
-    onBackground = Color(0xFF1C1B1F),
-    onSurface = Color(0xFF1C1B1F),
-    */
 )
 
 @Composable
