1c71f9328a9f84a2ce63f4125590692988ae50f3
Rakhman <karymshakovv@gmail.com>
2025-05-20 20:25:18 +0200

    Add ProviderTypeClassifier object to determine ProviderType from api.

---
+++ app/src/main/java/com/currency/rateman/api/ProviderTypeClassifier.kt
@@ -0,0 +1,23 @@
+package com.currency.rateman.api
+
+import com.currency.rateman.data.model.ProviderType
+
+object ProviderTypeClassifier {
+    private val knownBanks = listOf(
+        "Komerční Banka",
+        "Česká Spořitelna",
+        "Moneta Money Bank",
+        "Raiffeisenbank",
+        "Unicredit Bank",
+        "Fio Banka",
+        "Air Bank"
+    )
+
+    fun determineProviderType(bankName: String): ProviderType {
+        return if (knownBanks.any { bankName.contains(it, ignoreCase = true) }) {
+            ProviderType.BANK
+        } else {
+            ProviderType.EXCHANGE
+        }
+    }
+}
