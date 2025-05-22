72cd65ba5cd9e72e04641effdfa97772fece5ab0
Rakhman <karymshakovv@gmail.com>
2025-05-22 21:30:38 +0200

    Remove unused `applicationScope` from `RateManApp`.

--- app/src/main/java/com/currency/rateman/RateManApp.kt
+++ app/src/main/java/com/currency/rateman/RateManApp.kt
@@ -2,16 +2,11 @@ package com.currency.rateman
 
 import android.app.Application
 import com.currency.rateman.di.appModule
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.launch
 import org.koin.android.ext.koin.androidContext
 import org.koin.core.component.KoinComponent
 import org.koin.core.context.startKoin
 
 class RateManApp: Application(), KoinComponent {
-    private val applicationScope = CoroutineScope(Dispatchers.IO)
-
     override fun onCreate() {
         super.onCreate()
         AppContainer.init(applicationContext)
