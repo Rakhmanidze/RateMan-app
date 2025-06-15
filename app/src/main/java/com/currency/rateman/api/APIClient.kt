ad6f66ecb24b3a2dc8f0c88b3a24e1f04ea52309
Rakhman <karymshakovv@gmail.com>
2025-06-15 18:09:15 +0200

    Remove HttpLoggingInterceptor from APIClient.kt.

--- app/src/main/java/com/currency/rateman/api/APIClient.kt
+++ app/src/main/java/com/currency/rateman/api/APIClient.kt
@@ -3,17 +3,12 @@ package com.currency.rateman.api
 import kotlinx.serialization.json.Json
 import okhttp3.MediaType.Companion.toMediaType
 import okhttp3.OkHttpClient
-import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.kotlinx.serialization.asConverterFactory
 
 object APIClient {
 
     private const val BASE_URL = "https://data.kurzy.cz/"
-
-    private val logging = HttpLoggingInterceptor().apply {
-        setLevel(HttpLoggingInterceptor.Level.BODY)
-    }
 
     val okHttpClient = OkHttpClient.Builder()
         .build()
