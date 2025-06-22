af58b4a2d398ee76c0855831baf1f99956655ab6
Rakhman <karymshakovv@gmail.com>
2025-06-22 16:17:37 +0200

    Sort exchange rates alphabetically by currency code.
    The `rates` list within the `Provider` object is now sorted based on the order of `CurrencyCode` enum entries. This ensures a consistent and predictable display order for exchange rates.

--- app/src/main/java/com/currency/rateman/ui/viewmodels/ProviderViewModel.kt
+++ app/src/main/java/com/currency/rateman/ui/viewmodels/ProviderViewModel.kt
@@ -3,6 +3,7 @@ package com.currency.rateman.ui.viewmodels
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.currency.rateman.data.model.Provider
+import com.currency.rateman.data.model.enums.CurrencyCode
 import com.currency.rateman.data.repository.ProviderRepository
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
@@ -20,6 +21,12 @@ class ProviderViewModel(
         viewModelScope.launch {
             val provider = providerRepository.getProviderById(id)
             _provider.value = provider
+
+            _provider.value = provider?.copy(
+                rates = provider.rates.sortedBy { rate ->
+                    CurrencyCode.entries.toTypedArray().indexOfFirst { it == rate.foreignCurrency }
+                }
+            )
         }
     }
 }
