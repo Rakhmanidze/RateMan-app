845b7668b43ebd6acc1e0578ed7277c356bf598e
Rakhman <karymshakovv@gmail.com>
2025-06-28 13:08:23 +0200

    Remove done todo.

--- app/src/main/java/com/currency/rateman/ui/navigation/Routes.kt
+++ app/src/main/java/com/currency/rateman/ui/navigation/Routes.kt
@@ -22,7 +22,3 @@ sealed class Routes (val route: String) {
 //TODO: rename selectedCurrency variable to targetCurrency in filter entity and class
 // after it version of db increase by 1TODO: rename all default to
 // base in all languages which i have
-
-//TODO: in base currency screen have title saying choose base currency and in targetcurrency
-// screen say choose target or something instead of target but not just choose currency
-
