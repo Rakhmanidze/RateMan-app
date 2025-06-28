92a5f4683074087fd5851dc5f8334ca20817f768
Rakhman <karymshakovv@gmail.com>
2025-06-28 12:05:10 +0200

    Add todo.

--- app/src/main/java/com/currency/rateman/ui/navigation/Routes.kt
+++ app/src/main/java/com/currency/rateman/ui/navigation/Routes.kt
@@ -19,3 +19,7 @@ sealed class Routes (val route: String) {
 //TODO:Rename default currency to base currency to more clear name.
 // after it version of db increase by 1TODO: rename all default to
 // base in all languages which i have
+
+//TODO: Rename CurrencyScreen to TargetCurrency and variable currency to targetCurrency
+
+//TODO: in base currency screen have title saying choose base currency and in targetcurrency screen say choose target or something instead of target but not just choose currency
