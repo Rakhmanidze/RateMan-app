35e5b051b8589ef15fcf14eea68c797538c02e81
Rakhman <karymshakovv@gmail.com>
2025-07-05 17:41:41 +0200

    Reorder `AURA_AKTIV` constant alphabetically in `ProviderConstants.kt`.

--- app/src/main/java/com/currency/rateman/utils/ProviderConstants.kt
+++ app/src/main/java/com/currency/rateman/utils/ProviderConstants.kt
@@ -30,23 +30,23 @@ object ProviderConstants {
 
     object Scraped_providers {
         const val ALFA_PRAGUE = "Alfa Prague"
+        const val AURA_AKTIV = "Aura Aktiv"
         const val CERNA_RUZE_EXCHANGE = "Černá Růže Exchange"
         const val EURO_CHANGE = "Euro Change"
         const val EXCHANGE8 = "Exchange8"
         const val JINDRISSKA_EXCHANGE = "Jindřišská Exchange"
         const val ROYAL_EXCHANGE = "Royal Exchange"
         const val TOP_EXCHANGE = "Top Exchange"
-        const val AURA_AKTIV = "Aura Aktiv"
     }
 
     object Urls {
         const val ALFA_PRAGUE = "https://www.alfaprague.cz/web2/"
+        const val AURA_AKTIV = "https://smenarna-praha-1.cz/en/"
         const val CERNA_RUZE_EXCHANGE = "https://www.cernaruze-exchange.cz/"
         const val EURO_CHANGE = "https://www.eurochange.cz/"
         const val EXCHANGE8 = "https://www.exchange8.cz/"
         const val JINDRISSKA_EXCHANGE = "https://jindrisska-exchange.cz/"
         const val ROYAL_EXCHANGE = "https://www.royalexchange.cz/"
         const val TOP_EXCHANGE = "https://top-exchange.cz/"
-        const val AURA_AKTIV = "https://smenarna-praha-1.cz/en/"
     }
 }
