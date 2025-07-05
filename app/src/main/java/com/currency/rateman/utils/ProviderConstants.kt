3afbcd73c1aff3d228ed880b5ad7181580bf9786
Rakhman <karymshakovv@gmail.com>
2025-07-05 16:43:09 +0200

    Reorder provider lists alphabetically.
    The `EXCLUDED_PROVIDERS`, `KNOWN_BANKS`, `Scraped_providers`, and `Urls` lists in `ProviderConstants.kt` have been reordered alphabetically for consistency.

--- app/src/main/java/com/currency/rateman/utils/ProviderConstants.kt
+++ app/src/main/java/com/currency/rateman/utils/ProviderConstants.kt
@@ -2,49 +2,49 @@ package com.currency.rateman.utils
 
 object ProviderConstants {
     val EXCLUDED_PROVIDERS = listOf(
-        "Turecká centrální banka",
-        "Poštovní spořitelna",
-        "Prepocet EURa",
-        "Exchange",
+        "AKCENTA CZ",
         "Česká národní banka",
         "Citfin",
-        "AKCENTA CZ",
-        "RoklenFX"
+        "Exchange",
+        "Poštovní spořitelna",
+        "Prepocet EURa",
+        "RoklenFX",
+        "Turecká centrální banka"
     )
 
     val KNOWN_BANKS = listOf(
-        "Komerční Banka",
-        "Česká Spořitelna",
-        "Moneta Money Bank",
-        "Raiffeisenbank",
-        "Unicredit Bank",
-        "Fio Banka",
         "Air Bank",
-        "MONETA",
-        "Max banka",
+        "Česká Spořitelna",
         "ČSOB",
+        "Fio Banka",
+        "Komerční Banka",
+        "Max banka",
         "mBank",
+        "Moneta Money Bank",
+        "MONETA",
+        "Oberbank AG",
+        "Raiffeisenbank",
         "Trinity Bank",
-        "Oberbank AG"
+        "Unicredit Bank"
     )
 
     object Scraped_providers {
-        const val TOP_EXCHANGE = "Top Exchange"
         const val ALFA_PRAGUE = "Alfa Prague"
-        const val JINDRISSKA_EXCHANGE = "Jindřišská Exchange"
-        const val EURO_CHANGE = "Euro Change"
-        const val ROYAL_EXCHANGE = "Royal Exchange"
         const val CERNA_RUZE_EXCHANGE = "Černá Růže Exchange"
+        const val EURO_CHANGE = "Euro Change"
         const val EXCHANGE8 = "Exchange8"
+        const val JINDRISSKA_EXCHANGE = "Jindřišská Exchange"
+        const val ROYAL_EXCHANGE = "Royal Exchange"
+        const val TOP_EXCHANGE = "Top Exchange"
     }
 
     object Urls {
         const val ALFA_PRAGUE = "https://www.alfaprague.cz/web2/"
-        const val JINDRISSKA_EXCHANGE = "https://jindrisska-exchange.cz/"
-        const val TOP_EXCHANGE = "https://top-exchange.cz/"
-        const val EURO_CHANGE = "https://www.eurochange.cz/"
-        const val ROYAL_EXCHANGE = "https://www.royalexchange.cz/"
         const val CERNA_RUZE_EXCHANGE = "https://www.cernaruze-exchange.cz/"
+        const val EURO_CHANGE = "https://www.eurochange.cz/"
         const val EXCHANGE8 = "https://www.exchange8.cz/"
+        const val JINDRISSKA_EXCHANGE = "https://jindrisska-exchange.cz/"
+        const val ROYAL_EXCHANGE = "https://www.royalexchange.cz/"
+        const val TOP_EXCHANGE = "https://top-exchange.cz/"
     }
 }
