9cd69f0103698e74931b05a9a91a2d8824e59c25
Rakhman <karymshakovv@gmail.com>
2025-08-03 18:15:03 +0200

    Implement `GetAllProviderUseCase`.
    This use case retrieves all providers from the `ProviderRepository`.

--- app/src/main/java/com/currency/rateman/provider/domain/usecase/GetAllProviderUseCase.kt
+++ app/src/main/java/com/currency/rateman/provider/domain/usecase/GetAllProviderUseCase.kt
@@ -1,4 +1,8 @@
 package com.currency.rateman.provider.domain.usecase
 
+import com.currency.rateman.provider.data.model.Provider
+import kotlinx.coroutines.flow.Flow
+
 interface GetAllProviderUseCase {
+    fun execute(): Flow<List<Provider>>
 }
--- app/src/main/java/com/currency/rateman/provider/domain/usecase/GetAllProviderUseCaseImpl.kt
+++ app/src/main/java/com/currency/rateman/provider/domain/usecase/GetAllProviderUseCaseImpl.kt
@@ -1,8 +1,12 @@
 package com.currency.rateman.provider.domain.usecase
 
+import com.currency.rateman.provider.data.model.Provider
 import com.currency.rateman.provider.domain.repository.ProviderRepository
+import kotlinx.coroutines.flow.Flow
 
 class GetAllProviderUseCaseImpl (
     private val providerRepository: ProviderRepository) : GetAllProviderUseCase {
-
+    override fun execute(): Flow<List<Provider>> {
+        return providerRepository.getAllProviders()
+    }
 }
