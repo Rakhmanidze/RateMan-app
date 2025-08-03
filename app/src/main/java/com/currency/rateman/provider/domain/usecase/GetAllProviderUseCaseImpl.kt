126f0a72b7ac6306b086f4356a1979f82cc2234d
Rakhman <karymshakovv@gmail.com>
2025-08-03 15:30:35 +0200

    Implement GetAllProviderUseCase interface in GetAllProviderUseCaseImpl.

--- app/src/main/java/com/currency/rateman/provider/domain/usecase/GetAllProviderUseCaseImpl.kt
+++ app/src/main/java/com/currency/rateman/provider/domain/usecase/GetAllProviderUseCaseImpl.kt
@@ -3,6 +3,6 @@ package com.currency.rateman.provider.domain.usecase
 import com.currency.rateman.provider.domain.repository.ProviderRepository
 
 class GetAllProviderUseCaseImpl (
-    private val providerRepository: ProviderRepository) {
+    private val providerRepository: ProviderRepository) : GetAllProviderUseCase {
 
 }
