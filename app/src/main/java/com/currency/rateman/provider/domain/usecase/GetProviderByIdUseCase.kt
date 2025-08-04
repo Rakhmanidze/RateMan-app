b3a4f3e9dfcb121c85a0a4ad2b0fedbca6ae08bd
Rakhman <karymshakovv@gmail.com>
2025-08-04 13:37:57 +0200

    Implement `execute` method in `GetProviderByIdUseCase`.
    The `execute` method is added to the `GetProviderByIdUseCase` interface and implemented in `GetProviderByIdUseCaseImpl`. The implementation currently throws a `NotImplementedError`.

--- app/src/main/java/com/currency/rateman/provider/domain/usecase/GetProviderByIdUseCase.kt
+++ app/src/main/java/com/currency/rateman/provider/domain/usecase/GetProviderByIdUseCase.kt
@@ -1,4 +1,8 @@
 package com.currency.rateman.provider.domain.usecase
 
+import com.currency.rateman.provider.data.model.Provider
+import kotlinx.coroutines.flow.Flow
+
 interface GetProviderByIdUseCase {
+    fun execute(): Flow<Provider>
 }
--- app/src/main/java/com/currency/rateman/provider/domain/usecase/GetProviderByIdUseCaseImpl.kt
+++ app/src/main/java/com/currency/rateman/provider/domain/usecase/GetProviderByIdUseCaseImpl.kt
@@ -1,7 +1,12 @@
 package com.currency.rateman.provider.domain.usecase
 
+import com.currency.rateman.provider.data.model.Provider
 import com.currency.rateman.provider.domain.repository.ProviderRepository
+import kotlinx.coroutines.flow.Flow
 
 class GetProviderByIdUseCaseImpl (
     private val providerRepository: ProviderRepository) : GetProviderByIdUseCase {
+    override fun execute(): Flow<Provider> {
+        TODO("Not yet implemented")
+    }
 }
