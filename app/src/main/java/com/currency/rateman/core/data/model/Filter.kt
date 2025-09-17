9d56a0769ccf1cd4d97b23011b2c5194c719369a
Rakhman <karymshakovv@gmail.com>
2025-09-17 14:02:02 +0200

    Move enum ProviderType from core into provider package.

--- app/src/main/java/com/currency/rateman/core/data/model/Filter.kt
+++ app/src/main/java/com/currency/rateman/core/data/model/Filter.kt
@@ -2,7 +2,7 @@ package com.currency.rateman.core.data.model
 
 import android.os.Parcelable
 import com.currency.rateman.core.data.model.enums.CurrencyCode
-import com.currency.rateman.core.data.model.enums.ProviderType
+import com.currency.rateman.provider.data.model.enums.ProviderType
 import com.currency.rateman.core.data.model.enums.RateSortType
 import kotlinx.parcelize.Parcelize
 import kotlinx.serialization.Serializable
