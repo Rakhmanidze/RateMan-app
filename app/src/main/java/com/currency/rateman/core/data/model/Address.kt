d60efff9e91d47a1e66c644795346eb38f0ba0e2
Rakhman <karymshakovv@gmail.com>
2025-09-17 14:03:43 +0200

    Remove unused Address data class.

--- app/src/main/java/com/currency/rateman/core/data/model/Address.kt
+++
@@ -1,9 +0,0 @@
-package com.currency.rateman.core.data.model
-
-data class Address(
-    val street: String,
-    val buildingNumber: String,
-    val city: String,
-    val postalCode: String,
-    val country: String
-)
