56146a0d79279cc061afb3ba2916e6db0b7fccd8
Rakhman <karymshakovv@gmail.com>
2025-09-18 16:15:23 +0200

    Delete unused files.

--- app/src/main/java/com/currency/rateman/core/domain/usecase/GetSettingsUseCase.kt
+++
@@ -1,8 +0,0 @@
-package com.currency.rateman.core.domain.usecase
-
-import com.currency.rateman.core.data.model.Settings
-import kotlinx.coroutines.flow.Flow
-
-interface GetSettingsUseCase {
-    suspend fun execute(): Flow<Settings>
-}
