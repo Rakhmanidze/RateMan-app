9a876b07d6eeb7440cb4689e2f51e68059cf764d
Rakhman <karymshakovv@gmail.com>
2025-09-22 21:27:19 +0200

    Remove () from `GetAllCurrenciesUseCaseImpl`.

--- app/src/main/java/com/currency/rateman/core/domain/usecase/GetAllCurrenciesUseCaseImpl.kt
+++ app/src/main/java/com/currency/rateman/core/domain/usecase/GetAllCurrenciesUseCaseImpl.kt
@@ -2,7 +2,7 @@ package com.currency.rateman.core.domain.usecase
 
 import com.currency.rateman.core.data.model.enums.CurrencyCode
 
-class GetAllCurrenciesUseCaseImpl() : GetAllCurrenciesUseCase {
+class GetAllCurrenciesUseCaseImpl : GetAllCurrenciesUseCase {
     override fun execute(): List<CurrencyCode> {
         return CurrencyCode.entries.toList()
     }
