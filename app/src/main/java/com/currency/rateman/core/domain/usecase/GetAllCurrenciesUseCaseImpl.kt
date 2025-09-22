8b18d5d769591fd388772a424d6ef7ca2f8cb7a8
Rakhman <karymshakovv@gmail.com>
2025-09-22 13:04:55 +0200

    Implement GetAllCurrenciesUseCaseImpl.

--- app/src/main/java/com/currency/rateman/core/domain/usecase/GetAllCurrenciesUseCaseImpl.kt
+++ app/src/main/java/com/currency/rateman/core/domain/usecase/GetAllCurrenciesUseCaseImpl.kt
@@ -1,4 +1,9 @@
 package com.currency.rateman.core.domain.usecase
 
-class GetAllCurrenciesUseCaseImpl {
+import com.currency.rateman.core.data.model.enums.CurrencyCode
+
+class GetAllCurrenciesUseCaseImpl() : GetAllCurrenciesUseCase {
+    override fun execute(): List<CurrencyCode> {
+        return CurrencyCode.entries.toList()
+    }
 }
