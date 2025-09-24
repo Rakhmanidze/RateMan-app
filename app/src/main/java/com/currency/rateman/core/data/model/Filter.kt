0fb978da46b3b6a65a44508c7d744baace7c9859
Rakhman <karymshakovv@gmail.com>
2025-09-24 16:18:05 +0200

    Remove Parcelable and Serializable from Filter data class.
    The Filter data class no longer implements Parcelable and is not annotated with @Serializable.

--- app/src/main/java/com/currency/rateman/core/data/model/Filter.kt
+++ app/src/main/java/com/currency/rateman/core/data/model/Filter.kt
@@ -1,16 +1,11 @@
 package com.currency.rateman.core.data.model
 
-import android.os.Parcelable
 import com.currency.rateman.core.domain.model.CurrencyCode
 import com.currency.rateman.provider.data.model.ProviderType
 import com.currency.rateman.core.domain.model.RateSortType
-import kotlinx.parcelize.Parcelize
-import kotlinx.serialization.Serializable
 
-@Parcelize
-@Serializable
 data class Filter(
     val selectedProviderType: ProviderType,
     val targetCurrency: CurrencyCode,
     val selectedRateSortType: RateSortType
-) : Parcelable
+)
