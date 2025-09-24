bb4a534bc242a00351f404995bea810a8534607a
Rakhman <karymshakovv@gmail.com>
2025-09-24 16:18:52 +0200

    Remove Parcelable and Serializable from Settings data class.
    The `Settings` data class no longer implements `Parcelable` and is not annotated with `@Serializable`. This change simplifies the class as these functionalities were not being utilized.

--- app/src/main/java/com/currency/rateman/core/data/model/Settings.kt
+++ app/src/main/java/com/currency/rateman/core/data/model/Settings.kt
@@ -1,16 +1,11 @@
 package com.currency.rateman.core.data.model
 
-import android.os.Parcelable
 import com.currency.rateman.core.domain.model.CurrencyCode
 import com.currency.rateman.core.domain.app.LanguageCode
 import com.currency.rateman.core.domain.app.ThemeMode
-import kotlinx.parcelize.Parcelize
-import kotlinx.serialization.Serializable
 
-@Parcelize
-@Serializable
 data class Settings(
     val baseCurrency: CurrencyCode,
     val uiLanguage: LanguageCode,
     val themeMode: ThemeMode
-) : Parcelable
+)
