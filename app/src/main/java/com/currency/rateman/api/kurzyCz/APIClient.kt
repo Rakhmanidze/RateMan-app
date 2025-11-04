0609d09b4622afb7dd99bafae10f9638c3cb8c20
Rakhman <karymshakovv@gmail.com>
2025-11-04 19:37:06 +0100

    Remove commented out Retrofit client code.

--- app/src/main/java/com/currency/rateman/api/kurzyCz/APIClient.kt
+++ app/src/main/java/com/currency/rateman/api/kurzyCz/APIClient.kt
@@ -1,14 +1,8 @@
 package com.currency.rateman.api.kurzyCz
 
 import kotlinx.serialization.json.Json
-import okhttp3.MediaType.Companion.toMediaType
-import okhttp3.OkHttpClient
-import retrofit2.Retrofit
-import retrofit2.converter.kotlinx.serialization.asConverterFactory
-
 import io.ktor.client.*
 import io.ktor.client.call.*
-import io.ktor.client.engine.*
 import io.ktor.client.engine.okhttp.*
 import io.ktor.client.plugins.contentnegotiation.*
 import io.ktor.client.request.*
@@ -18,23 +12,11 @@ import io.ktor.serialization.kotlinx.json.*
 object APIClient {
     const val BASE_URL = "https://data.kurzy.cz/"
 
-//    val okHttpClient = OkHttpClient.Builder()
-//        .build()
-
     private val json = Json {
         ignoreUnknownKeys = true
         coerceInputValues = true
         isLenient = true
     }
-
-//    val ratesAPIService: RatesAPIService by lazy {
-//        Retrofit.Builder()
-//            .baseUrl(BASE_URL)
-//            .addConverterFactory(json.asConverterFactory("application/json".toMediaType()))
-//            .client(okHttpClient)
-//            .build()
-//            .create(RatesAPIService::class.java)
-//    }
 
     val httpClient = HttpClient(OkHttp) {
         install(ContentNegotiation) {
