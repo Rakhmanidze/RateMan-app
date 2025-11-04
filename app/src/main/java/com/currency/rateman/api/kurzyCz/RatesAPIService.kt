47a96ddbcd35f46bb27d921bcc353224eea5116e
Rakhman <karymshakovv@gmail.com>
2025-11-04 19:37:36 +0100

    Remove unused `RatesAPIService` for kurzy.cz.

--- app/src/main/java/com/currency/rateman/api/kurzyCz/RatesAPIService.kt
+++
@@ -1,8 +0,0 @@
-package com.currency.rateman.api.kurzyCz
-
-import retrofit2.http.GET
-
-interface RatesAPIService {
-    @GET("/json/meny/b%5B-1%5D.json")
-    suspend fun getExchangeRates(): List<ProviderAPI>
-}
