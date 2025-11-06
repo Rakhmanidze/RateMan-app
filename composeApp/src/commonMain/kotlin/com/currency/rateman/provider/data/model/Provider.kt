5731eb67cf719ab0edf61edbd65a213eb3a0a7ee
Rakhman <karymshakovv@gmail.com>
2025-11-07 00:35:27 +0100

    Move `Provider` data class to commonMain module.

--- composeApp/src/commonMain/kotlin/com/currency/rateman/provider/data/model/Provider.kt
+++ composeApp/src/commonMain/kotlin/com/currency/rateman/provider/data/model/Provider.kt
@@ -1,8 +1,7 @@
-package com.currency.rateman.provider.domain.model
+package com.currency.rateman.provider.data.model
 
 import com.currency.rateman.core.domain.model.CurrencyCode
 import com.currency.rateman.core.domain.model.CurrencyRate
-import com.currency.rateman.provider.data.model.ProviderType
 
 data class Provider(
     val id: Long,
