c8deaf5fe105b0838cea3be090cbcfa635d278b6
Rakhman <karymshakovv@gmail.com>
2025-11-11 22:32:14 +0100

    Create `PlatformModule.kt` for iOS with Ktor Darwin engine.

---
+++ composeApp/src/iosMain/kotlin/com/currency/rateman/di/PlatformModule.kt
@@ -0,0 +1,22 @@
+package com.currency.rateman.di
+
+import io.ktor.client.*
+import io.ktor.client.engine.darwin.*
+import io.ktor.client.plugins.contentnegotiation.*
+import io.ktor.serialization.kotlinx.json.*
+import kotlinx.serialization.json.Json
+import org.koin.dsl.module
+
+actual val platformModule = module {
+    factory<HttpClient> {
+        HttpClient(Darwin) {
+            install(ContentNegotiation) {
+                json(Json {
+                    ignoreUnknownKeys = true
+                    coerceInputValues = true
+                    isLenient = true
+                })
+            }
+        }
+    }
+}
