c745a351e532b373ab67392e1a7b00f8725ed56d
Rakhman <karymshakovv@gmail.com>
2025-11-13 18:49:46 +0100

    Move `SettingsDao` to commonMain module.

--- composeApp/src/commonMain/kotlin/com/currency/rateman/core/data/dao/SettingsDao.kt
+++ composeApp/src/commonMain/kotlin/com/currency/rateman/core/data/dao/SettingsDao.kt
@@ -1,11 +1,11 @@
 package com.currency.rateman.core.data.dao
 
+import com.currency.rateman.core.data.entity.SettingsEntity
 import androidx.room.Dao
 import androidx.room.Insert
 import androidx.room.OnConflictStrategy
 import androidx.room.Query
 import androidx.room.Update
-import com.currency.rateman.core.data.entity.SettingsEntity
 import kotlinx.coroutines.flow.Flow
 
 @Dao
