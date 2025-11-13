915a846f9f89576194b1c078ae69e053fd79416c
Rakhman <karymshakovv@gmail.com>
2025-11-13 19:28:22 +0100

    Remove not needed imports.

--- composeApp/src/androidMain/kotlin/com/currency/rateman/core/ui/component/ThemeSettingItem.kt
+++ composeApp/src/androidMain/kotlin/com/currency/rateman/core/ui/component/ThemeSettingItem.kt
@@ -1,17 +1,24 @@
 package com.currency.rateman.core.ui.component
 
+import androidx.annotation.DrawableRes
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.*
-import androidx.compose.material3.*
-import androidx.compose.runtime.*
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.unit.dp
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
-import androidx.annotation.DrawableRes
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
 import com.currency.rateman.R
 
 @Composable
