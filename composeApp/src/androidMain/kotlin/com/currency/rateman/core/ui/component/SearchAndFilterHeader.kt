ede654cf658bc68003a25fb63f46dcb1e72633b0
Rakhman <karymshakovv@gmail.com>
2025-11-13 19:27:30 +0100

    Reorder imports in `SearchAndFilterHeader.kt`.

--- composeApp/src/androidMain/kotlin/com/currency/rateman/core/ui/component/SearchAndFilterHeader.kt
+++ composeApp/src/androidMain/kotlin/com/currency/rateman/core/ui/component/SearchAndFilterHeader.kt
@@ -1,39 +1,44 @@
 package com.currency.rateman.core.ui.component
 
-import com.currency.rateman.R as AppR
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.unit.dp
-import androidx.compose.runtime.getValue
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.*
-import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.res.stringResource
-import androidx.navigation.NavHostController
-import com.currency.rateman.core.ui.navigation.Routes
 import androidx.compose.foundation.horizontalScroll
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.IntrinsicSize
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.layout.wrapContentWidth
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.DropdownMenu
 import androidx.compose.material3.DropdownMenuItem
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedButton
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.input.TextFieldValue
+import androidx.compose.ui.unit.dp
+import androidx.navigation.NavHostController
 import com.currency.rateman.core.domain.model.CurrencyCode
-import com.currency.rateman.provider.domain.model.ProviderType
 import com.currency.rateman.core.domain.model.RateSortType
+import com.currency.rateman.core.ui.navigation.Routes
+import com.currency.rateman.provider.domain.model.ProviderType
+import com.currency.rateman.R as AppR
 
 @Composable
 fun SearchAndFilterHeader(
