30309778f1713227b1961b6e387450c4baeb2741
Rakhman <karymshakovv@gmail.com>
2025-11-14 15:24:11 +0100

    Remove unused imports.

--- composeApp/src/androidMain/kotlin/com/currency/rateman/RateManApp.kt
+++ composeApp/src/androidMain/kotlin/com/currency/rateman/RateManApp.kt
@@ -1,12 +1,12 @@
 package com.currency.rateman
 
 import android.app.Application
+import com.currency.rateman.di.AppContainer
 import com.currency.rateman.di.appModule
+import com.currency.rateman.di.platformModule
 import org.koin.android.ext.koin.androidContext
 import org.koin.core.component.KoinComponent
 import org.koin.core.context.startKoin
-import com.currency.rateman.di.AppContainer
-import com.currency.rateman.di.platformModule
 
 class RateManApp: Application(), KoinComponent {
     override fun onCreate() {
