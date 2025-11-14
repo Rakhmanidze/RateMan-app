396dc719b8e500a120a23034460508c919c9712d
Rakhman <karymshakovv@gmail.com>
2025-11-14 15:20:55 +0100

    Reorder imports alphabetically.

--- composeApp/src/androidMain/kotlin/com/currency/rateman/core/ui/screen/SettingsScreen.kt
+++ composeApp/src/androidMain/kotlin/com/currency/rateman/core/ui/screen/SettingsScreen.kt
@@ -1,27 +1,29 @@
 package com.currency.rateman.core.ui.screen
 
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.material3.*
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.unit.dp
-import com.currency.rateman.R
-import com.currency.rateman.core.ui.navigation.BottomNavItem
-import com.currency.rateman.core.domain.app.ThemeMode
-import com.currency.rateman.core.ui.component.LanguageSettingItem
-import com.currency.rateman.core.ui.viewmodel.SettingsViewModel
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavHostController
+import com.currency.rateman.R
+import com.currency.rateman.core.domain.app.ThemeMode
 import com.currency.rateman.core.ui.component.BottomNavBar
 import com.currency.rateman.core.ui.component.CurrencySettingItem
+import com.currency.rateman.core.ui.component.LanguageSettingItem
 import com.currency.rateman.core.ui.component.ThemeSettingItem
+import com.currency.rateman.core.ui.navigation.BottomNavItem
+import com.currency.rateman.core.ui.viewmodel.SettingsViewModel
 import com.currency.rateman.di.navigation.sharedKoinNavViewModel
 import java.util.Locale
 
