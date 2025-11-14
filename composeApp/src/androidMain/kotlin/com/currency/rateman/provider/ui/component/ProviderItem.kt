0a49eb57b98d9aedb7e91570d375dc0e70a0ac69
Rakhman <karymshakovv@gmail.com>
2025-11-14 15:25:42 +0100

    Remove unused import.

--- composeApp/src/androidMain/kotlin/com/currency/rateman/provider/ui/component/ProviderItem.kt
+++ composeApp/src/androidMain/kotlin/com/currency/rateman/provider/ui/component/ProviderItem.kt
@@ -17,8 +17,8 @@ import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
 import com.currency.rateman.core.domain.model.CurrencyRate
-import com.currency.rateman.provider.domain.model.Provider
 import com.currency.rateman.core.utils.formatRate
+import com.currency.rateman.provider.domain.model.Provider
 
 @Composable
 fun ProviderItem(
