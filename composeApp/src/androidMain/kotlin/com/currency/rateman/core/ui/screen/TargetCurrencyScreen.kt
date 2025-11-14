807dc63a31e561c8487cdb612c83113f3046f994
Rakhman <karymshakovv@gmail.com>
2025-11-14 15:21:25 +0100

    Reorder imports.

--- composeApp/src/androidMain/kotlin/com/currency/rateman/core/ui/screen/TargetCurrencyScreen.kt
+++ composeApp/src/androidMain/kotlin/com/currency/rateman/core/ui/screen/TargetCurrencyScreen.kt
@@ -24,12 +24,12 @@ import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.unit.dp
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavHostController
 import com.currency.rateman.R
 import com.currency.rateman.core.ui.component.SearchInput
