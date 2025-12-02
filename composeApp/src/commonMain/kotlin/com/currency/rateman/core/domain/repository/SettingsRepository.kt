d2ad8776de393c605be8f8356cbfb73b29ba07ee
Rakhman <karymshakovv@gmail.com>
2025-12-02 13:07:10 +0100

    Update `SettingsRepository` to remove language code parameter from `editSettings`.

--- composeApp/src/commonMain/kotlin/com/currency/rateman/core/domain/repository/SettingsRepository.kt
+++ composeApp/src/commonMain/kotlin/com/currency/rateman/core/domain/repository/SettingsRepository.kt
@@ -1,6 +1,5 @@
 package com.currency.rateman.core.domain.repository
 
-import com.currency.rateman.core.domain.app.LanguageCode
 import com.currency.rateman.core.domain.model.CurrencyCode
 import com.currency.rateman.core.domain.model.Settings
 import kotlinx.coroutines.flow.Flow
@@ -9,8 +8,7 @@ interface SettingsRepository {
     fun getSettings(): Flow<Settings>
 
     suspend fun editSettings(
-        currencyCode: CurrencyCode? = null,
-        languageCode: LanguageCode? = null
+        currencyCode: CurrencyCode? = null
     )
 
     suspend fun ensureSettingsExist()
