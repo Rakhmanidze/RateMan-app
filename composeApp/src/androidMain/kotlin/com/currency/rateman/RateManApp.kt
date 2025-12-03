68aa70507f6203161e906201c3b576dfdd3c1f02
Rakhman <karymshakovv@gmail.com>
2025-12-03 14:13:45 +0100

    Remove commented out `startKoin` code and unused imports in `RateManApp.kt`.

--- composeApp/src/androidMain/kotlin/com/currency/rateman/RateManApp.kt
+++ composeApp/src/androidMain/kotlin/com/currency/rateman/RateManApp.kt
@@ -1,12 +1,9 @@
 package com.currency.rateman
 
 import android.app.Application
-import com.currency.rateman.di.appModule
 import com.currency.rateman.di.initKoin
-import com.currency.rateman.di.platformModule
 import org.koin.android.ext.koin.androidContext
 import org.koin.core.component.KoinComponent
-import org.koin.core.context.startKoin
 
 class RateManApp: Application(), KoinComponent {
     override fun onCreate() {
@@ -14,9 +11,5 @@ class RateManApp: Application(), KoinComponent {
         initKoin {
             androidContext(this@RateManApp)
         }
-//        startKoin {
-//            androidContext(this@RateManApp)
-//            modules(platformModule, appModule)
-//        }
     }
 }
